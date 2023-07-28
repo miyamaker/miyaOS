5836809750199011ce286109706d988aa07590ee
♡ miyamaker ♡ <miyacyberangel@proton.me>
2023-07-28 12:04:25 +0000

    1inch widget baby

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -1,5 +1,5 @@
 import { setup1inchWidget } from '@1inch/embedded-widget'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import styled from 'styled-components/macro'
 
 import TitleBar from '@/components/TitleBar'
@@ -15,14 +15,32 @@ const TextContainer = styled.div`
   padding: 8px;
   font-size: 14px;
 `
-
-const ResponsiveImage = styled.img`
-  ${({ theme }) => theme.mediaWidth.upToMedium`
-  display: none;
-`}
+const OneInchWindow = styled.div`
+  height: 100%;
+  width: 100%;
+  iframe {
+    height: 95%;
+    width: 100%;
+  }
 `
 
 export default function MintPage() {
+  const hostRef = useRef(null)
+
+  useEffect(() => {
+    if (hostRef.current) {
+      const iframeJsonRpcManager = setup1inchWidget({
+        chainId: 1,
+        sourceTokenSymbol: '1INCH',
+        destinationTokenSymbol: '0xa358491CA72B793ddf21cF46C7289CC6e0ce9e5A',
+        hostElement: hostRef.current,
+        provider: window.ethereum,
+        theme: 'light',
+        sourceTokenAmount: '15',
+      })
+    }
+  }, [])
+
   const [fullscreen, toggleFullscreen] = useFullscreen('swap')
   const dispatch = useAppDispatch()
   const close = () => dispatch(closeWindow({ value: 'swap' }))
@@ -30,16 +48,6 @@ export default function MintPage() {
 
   const [inputValue, setInputValue] = useState('')
   const [error, setError] = useState(false)
-
-  const iframeJsonRpcManager = setup1inchWidget({
-    chainId: 1,
-    sourceTokenSymbol: 'ETH',
-    destinationTokenSymbol: '0xa358491CA72B793ddf21cF46C7289CC6e0ce9e5A',
-    hostElement: document.body,
-    provider: window.ethereum,
-    theme: 'dark',
-    sourceTokenAmount: '.1',
-  })
 
   return (
     <WindowWrapper>
@@ -60,11 +68,11 @@ export default function MintPage() {
       >
         {page?.label}
       </TitleBar>
-      <div style={{ display: 'flex', flexDirection: 'column', padding: '0.5rem', gap: '1rem' }}>
-        <div style={{ display: 'flex', marginBottom: '0.5rem' }}>
-          <TextContainer>{}</TextContainer>
-        </div>
-      </div>
+      {/* <div style={{ display: 'flex', flexDirection: 'column', padding: '0.5rem', gap: '1rem', width: '100%' }}> */}
+      {/* <div style={{ display: 'flex', marginBottom: '0.5rem', width: '100%' }}> */}
+      <OneInchWindow ref={hostRef} />
+      {/* </div> */}
+      {/* </div> */}
     </WindowWrapper>
   )
 }
