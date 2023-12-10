1a50a562a05fc8ade0687a25649828f5dc5f130c
miyamaker <azp@Kilamatronic.local>
2023-12-09 21:05:23 -0500

    adding ga

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,9 @@ import { HashRouter } from 'react-router-dom'
 
 import App from './App'
 import { GlobalHistory } from './GlobalHistory'
+import ReactGA from "react-ga4";
 
+ReactGA.initialize("G-W08254B3RN");
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const rootElement = document.getElementById('app')!
 const root = ReactDOM.createRoot(rootElement)
